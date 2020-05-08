3238ce0a03239c80664b5ce798f2d78eeacfb20c
bugagashenkj <bugagashenkj@gmail.com>
2020-05-08 20:49:13 +0300

    Add unit/integration tests

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,7 +1,43 @@
 package main
 
-import "testing"
+import (
+	"math"
+	"math/rand"
+	"testing"
+	. "gopkg.in/check.v1"
+)
 
-func TestBalancer(t *testing.T) {
-	// TODO: Реалізуйте юніт-тест для балансувальникка.
+func Test(t *testing.T) { TestingT(t) }
+
+type MySuite struct{}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func randStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func (s *MySuite) TestBalancer(c *C) {
+	counts := 10000
+	serversCount := make(map[string]int, len(serversPool.pool))
+
+	for _, server := range serversPool.pool {
+		serversCount[server] = 0
+	}
+
+	for i := 0; i < counts; i++ {
+		randURL := randStringBytes(20)
+		server := getServerByURL(randURL)
+		serversCount[server]++
+	}
+
+	expectedRatio := 1.0 / float64(len(serversPool.pool))
+	for _, count := range serversCount {
+		diff := math.Abs(expectedRatio - float64(count)/float64(counts))
+		c.Assert(diff > 0.05, Equals, true)
+	}
 }
--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -3,8 +3,10 @@ package integration
 import (
 	"fmt"
 	"net/http"
+	"sync"
 	"testing"
 	"time"
+	"math/rand"
 )
 
 const baseAddress = "http://balancer:8090"
@@ -13,15 +15,65 @@ var client = http.Client{
 	Timeout: 3 * time.Second,
 }
 
-func TestBalancer(t *testing.T) {
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
+var serversPool = []string{
+	"http://localhost:8080",
+	"http://localhost:8081",
+	"http://localhost:8082",
+}
+
+type ServerCounts struct {
+	mutex sync.Mutex
+	counters map[string]int
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func randStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+	return string(b)
+}
+
+func worker(wg *sync.WaitGroup, client http.Client, serverCounts *ServerCounts, i int, t *testing.T) {
+	defer wg.Done()
+	randUrl := randStringBytes(10)
+	resp, err := client.Get(fmt.Sprintf("%s/%s", baseAddress, randUrl))
+	respServer := resp.Header.Get("lb-from")
+
+	if err != nil {
+		t.Logf("response from [%s]", respServer)
+	}
+
+	serverCounts.mutex.Lock()
+	serverCounts.counters[respServer] += 1
+	serverCounts.mutex.Unlock()
+}
+
+func TestBalancer(t *testing.T) {
+	m := ServerCounts{counters: make(map[string]int)}
+	var wg sync.WaitGroup
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go worker(&wg, client, &m, i, t)
+	}
+	wg.Wait()
 }
 
 func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+	var wg sync.WaitGroup
+	for n := 0; n < b.N; n++ {
+		wg.Add(1)
+		go func(group sync.WaitGroup) {
+			defer wg.Done()
+			randUrl := randStringBytes(10)
+			_, err := client.Get(fmt.Sprintf("%s/%s", baseAddress, randUrl))
+			if err != nil {
+				b.Error(err)
+			}
+		}(wg)
+	}
+	wg.Wait()
 }
+
